e078799bfa03104c64ef0d26a962b8e7137381a5
Saif <saifad303@gmail.com>
2023-05-30 14:34:51 +0600

    add cdn and script link to index.html

--- src/pages/adminPages/RootAdmin.jsx
+++ src/pages/adminPages/RootAdmin.jsx
@@ -49,8 +49,20 @@ const RootAdmin = () => {
                 </span>
               </Link>
               <Link to="/" className="flex ml-2 md:mr-24">
-                <span className="self-center text-xl font-semibold whitespace-nowrap  bg-green-800 text-white py-2 px-4 rounded-lg">
+                <span className="self-center hidden sm:inline text-xl font-semibold whitespace-nowrap  bg-green-800 text-white py-2 px-4 rounded-lg">
                   Go Home
+                </span>
+                <span className="self-center sm:hidden text-xl font-semibold whitespace-nowrap  bg-green-800 text-white py-2 px-4 rounded-lg">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 50 50"
+                    width="20px"
+                    height="20px"
+                    fill="white"
+                  >
+                    {" "}
+                    <path d="M 25 1.0507812 C 24.7825 1.0507812 24.565859 1.1197656 24.380859 1.2597656 L 1.3808594 19.210938 C 0.95085938 19.550938 0.8709375 20.179141 1.2109375 20.619141 C 1.5509375 21.049141 2.1791406 21.129062 2.6191406 20.789062 L 4 19.710938 L 4 46 C 4 46.55 4.45 47 5 47 L 19 47 L 19 29 L 31 29 L 31 47 L 45 47 C 45.55 47 46 46.55 46 46 L 46 19.710938 L 47.380859 20.789062 C 47.570859 20.929063 47.78 21 48 21 C 48.3 21 48.589063 20.869141 48.789062 20.619141 C 49.129063 20.179141 49.049141 19.550938 48.619141 19.210938 L 25.619141 1.2597656 C 25.434141 1.1197656 25.2175 1.0507812 25 1.0507812 z M 35 5 L 35 6.0507812 L 41 10.730469 L 41 5 L 35 5 z" />
+                  </svg>
                 </span>
               </Link>
             </div>
@@ -60,7 +72,6 @@ const RootAdmin = () => {
                 <div>
                   <button
                     type="button"
-                    onClick={() => setIsProfileShow(!isProfileShow)}
                     className="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 "
                     aria-expanded="false"
                     data-dropdown-toggle="dropdown-user"
@@ -74,9 +85,7 @@ const RootAdmin = () => {
                   </button>
                 </div>
                 <div
-                  className={`absolute ${
-                    isProfileShow || "hidden"
-                  } right-0 top-6 z-50 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow`}
+                  className=" z-50 my-4 hidden text-base list-none bg-white divide-y divide-gray-100 rounded shadow"
                   id="dropdown-user"
                 >
                   <div className="px-4 py-3" role="none">
